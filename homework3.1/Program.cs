c875f8ca3aa1396cd3e962df9c3ca2669ab2cd8f
YuliyaVas <yulenka2596@gmail.com>
2022-08-08 01:43:01 +0300

    Добавьте файлы проекта.

---
+++ homework3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            int B = 10;
+
+            A = A + B;
+            B = A - B;
+            A = A - B;
+
+            Console.WriteLine("A=" + A);
+            Console.WriteLine("B=" + B);
+            Console.ReadKey();
+        }
+    }
+}
